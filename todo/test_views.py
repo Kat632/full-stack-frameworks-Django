cf682ef4336646417f7cb6fc16d35d0373ee4a79
Kat632 <katherine.biggs@mail.com>
2022-03-07 14:47:32 +0000

    add ability to delete a task

---
+++ todo/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+class TestDjango(TestCase):
+
+    def test_this_thing_works(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_works_1(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_works_2(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_works_3(self):
+        self.assertEqual(1, 1)
